103a1aca7c16518d31945f0d32b2fbef85e97586
secureanytime <jameswoodinfo@gmail.com>
2026-07-30 17:07:29 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,16 +2,25 @@ project = 'communities-anywhere'
 copyright = '2026'
 author = 'Admin'
 
-extensions = []
+extensions = [ 'sphinx.ext.autodoc',
+               'sphinx.ext.napoleon',
+               'sphinx_sitemap',
+              ]
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster' # Screenshot wala classic white theme
 
+html_baseurl = 'https://communities-anytime-activatebitdefender.readthedocs-hosted.com/en/latest/'
+sitemap_url_scheme = "{link}"
+
 # conf.py
 
 html_title = "Activate Bitdefender Ultimate Security"
 html_short_title = "Activate Bitdefender Ultimate Security"
+html_static_path = ['_static']
+html_extra_path = ['_static/google5ffeff63dcb91d99.html'] 
 
 
 # Meta Tags Configuration
